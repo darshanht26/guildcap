07ffdcc71719324c9836465c8356124fed9b31ac
darshanht26 <darshan@houndstoothsc.com>
2024-04-26 13:22:25 +0530

    update content & design fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,10 +36,12 @@ navLinks.forEach(navLink => {
   })
 })
 
+const scrollMarginTop = `${(siteHeader.offsetHeight + 20)}px`;
+
 // hash jump to section
 if (window.location.hash) {
   const targetElement = document.getElementById(window.location.hash.slice(1));
-  targetElement.style.scrollMarginTop = `${siteHeader.offsetHeight}px`
+  targetElement.style.scrollMarginTop = scrollMarginTop
 }
 
 const linksWithHash = document.querySelectorAll('a[href^="#"]');
@@ -47,7 +49,7 @@ linksWithHash.forEach(linkWithHash => {
   linkWithHash.addEventListener('click', (e) => {
     const hrefValue = e.currentTarget.getAttribute('href');;
     const targetElement = document.getElementById(hrefValue.slice(1));
-    targetElement.style.scrollMarginTop = `${siteHeader.offsetHeight}px`
+    targetElement.style.scrollMarginTop = scrollMarginTop
   })
 });
 
